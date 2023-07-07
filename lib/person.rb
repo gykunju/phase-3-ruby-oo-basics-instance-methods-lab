0d337cfb88b4105291382d682aa70e22f9830fed
Alvin <alvin.gikunju@student.moringaschool.com>
2023-07-07 17:13:55 +0300

    completed assignment

---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person 
+    def talk
+        puts "Hello Worls!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+end
+
+person1 = Person.new
+person.talk
+person.walk
